0425842928ba1735080112c322f10c1be31bb48c
Luqqk <mikolajczak.luq@gmail.com>
2021-06-30 00:24:51 +0200

    Add docstrings, fix bugs, redact README.md

--- README.md
+++ README.md
@@ -2,26 +2,55 @@
 
 Simple Python module for calculating bounding box (bbox) from given GeoJSON object.
 
+Currently following GeoJSON objects are supported ([RFC 7946](https://datatracker.ietf.org/doc/html/rfc7946)):
+
+1. [Point](https://datatracker.ietf.org/doc/html/rfc7946#section-3.1.2)
+2. [MultiPoint](https://datatracker.ietf.org/doc/html/rfc7946#section-3.1.3)
+3. [LineString](https://datatracker.ietf.org/doc/html/rfc7946#section-3.1.4)
+4. [MultiLineString](https://datatracker.ietf.org/doc/html/rfc7946#section-3.1.5)
+5. [Polygon](https://datatracker.ietf.org/doc/html/rfc7946#section-3.1.6)
+6. [MultiPolygon](https://datatracker.ietf.org/doc/html/rfc7946#section-3.1.7)
+7. [GeometryCollection](https://datatracker.ietf.org/doc/html/rfc7946#section-3.1.8)
+
 ### Installation
 
-In cloned repo directory:
-
 ```bash
-$ python setup.py install
+$ pip install gbbox
 ```
 
 ### Usage
 
 ```python
->>> import gbbox
->>> feature = gbbox.GeoJSON(geojson)
->>> feature_bbox = feature.bbox()
+>>> from gbbox import LineString
+>>>
+>>> linestring_geojson = {
+>>>     "type": "LineString",
+>>>     "coordinates": [
+>>>         [1.0, 2.0],
+>>>         [3.0, 4.0]
+>>>     ]
+>>> }
+>>>
+>>> linestring = Point(**linestring_geojson)
+>>> linestring_bbox = linestring.bbox()
 
-print feature_bbox
-[min_x, min_y, max_x, max_y]
+>>> print(linestring_bbox)
+>>> [1.0, 2.0, 3.0, 4.0]
+
+>>> print(linestring.min_lon)
+>>> 1.0
 ```
 
-### TODO
+### Development
 
-* add new features
-* write some tests
+```bash
+$ docker compose up -d
+# Start bash within container and enter it
+$ docker exec -it gbbox bash
+```
+
+Project will be automatically installed within docker container in an editable mode and
+any code changes will be immediately reflected. Keep in mind that if you have python shell
+running then you have to restart it.
+
+You can also use `make lint` and `make test` as shortcuts to run linters and tests
--- gbbox/objects.py
+++ gbbox/objects.py
@@ -7,38 +7,67 @@ from gbbox.constants import Position, GeometryType
 
 
 class GeoJSON(BaseModel):
+    """Base class that represents a GeoJSON object.
+
+    GeoJSON objects are implemented according to RFC 7946 specification:
+    https://datatracker.ietf.org/doc/html/rfc7946
+
+    Attributes:
+        type: GeoJSON object type
+        min_lon: Decimal number between -180.0 and 180.0 (bbox left)
+        min_lat: Decimal number between -90.0 and 90.0 (bbox bottom)
+        max_lon: Decimal number between -180.0 and 180.0 (bbox right)
+        max_lat: Decimal number between -90.0 and 90.0 (bbox top)
+    """
+
     type: GeometryType
 
     class Config:
-        keep_untouched = (cached_property,)
+        """Configures pydantic's behaviour"""
 
-    def __init__(self, geojson):
-        super().__init__(**geojson)
+        keep_untouched = (cached_property,)
 
     @cached_property
     def min_lon(self) -> float:
-        return min(self.flattened_coords[::2])
+        return min(self._flattened_coords[::2])
 
     @cached_property
     def min_lat(self) -> float:
-        return min(self.flattened_coords[1::2])
+        return min(self._flattened_coords[1::2])
 
     @cached_property
     def max_lon(self) -> float:
-        return max(self.flattened_coords[::2])
+        return max(self._flattened_coords[::2])
 
     @cached_property
     def max_lat(self) -> float:
-        return max(self.flattened_coords[1::2])
-
-    @cached_property
-    def flattened_coords(self) -> List[float]:
-        return list(self._chain_coords(self.coordinates))
+        return max(self._flattened_coords[1::2])
 
     def bbox(self) -> List[float]:
         return [self.min_lon, self.min_lat, self.max_lon, self.max_lat]
 
-    def _chain_coords(self, sequence) -> Iterable[float]:
+    @cached_property
+    def _flattened_coords(self) -> List[float]:
+        return list(self._chain_coords(self.coordinates))
+
+    def _chain_coords(self, sequence: Iterable) -> float:
+        """Generates flatten version of nested iterables.
+
+        This method (generator) works in a recursive fashion
+        and flattens Iterable until it is "exhausted".
+
+        Args:
+            sequence: Any object that is an Iterable. It can
+              be nested structure.
+
+        Returns:
+            Float object
+            within passed Iterable. For example:
+
+            [[1.0, 2.0], [3.0, 4.0]] will yield following values:
+
+            1.0, 2.0, 3.0, 4.0
+        """
         for val in sequence:
             if isinstance(val, Iterable):
                 for subval in self._chain_coords(val):
@@ -48,37 +77,59 @@ class GeoJSON(BaseModel):
 
 
 class Point(GeoJSON):
+    """Point GeoJSON object (RFC 7946)"""
+
     type: Literal[GeometryType.POINT.value]
     coordinates: Position
 
 
 class MultiPoint(GeoJSON):
+    """MultiPoint GeoJSON object (RFC 7946)"""
+
     type: Literal[GeometryType.MULTIPOINT.value]
     coordinates: conlist(Position, min_items=2)
 
 
 class LineString(GeoJSON):
+    """LineString GeoJSON object (RFC 7946)"""
+
     type: Literal[GeometryType.LINESTRING.value]
     coordinates: conlist(Position, min_items=2)
 
 
 class MultiLineString(GeoJSON):
+    """MultiLineString GeoJSON object (RFC 7946)"""
+
     type: Literal[GeometryType.MULTILINESTRING.value]
     coordinates: conlist(conlist(Position, min_items=2), min_items=2)
 
 
 class Polygon(GeoJSON):
+    """Polygon GeoJSON object (RFC 7946)"""
+
     type: Literal[GeometryType.POLYGON.value]
     coordinates: conlist(conlist(Position, min_items=4))
 
 
 class MultiPolygon(GeoJSON):
+    """MultiPolygon GeoJSON object (RFC 7946)"""
+
     type: Literal[GeometryType.MULTIPOLYGON.value]
     coordinates: conlist(conlist(conlist(Position, min_items=4)), min_items=2)
 
 
 class GeometryCollection(GeoJSON):
+    """GeometryCollection GeoJSON object (RFC 7946)"""
+
     type: Literal[GeometryType.GEOMETRYCOLLECTION.value]
     geometries: List[
         Union[Point, MultiPoint, LineString, MultiLineString, Polygon, MultiPolygon]
     ]
+
+    @cached_property
+    def _flattened_coords(self) -> List[float]:
+        coordinates = []
+        for object in self.geometries:
+            coordinates.append(object.coordinates)
+
+        return list(self._chain_coords(coordinates))
